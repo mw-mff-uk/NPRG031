aa92f541ab29123a2640232c2c4d9d1ba04facc3
Milan Wikarski <milan@wikarski.sk>
2020-03-04 14:26:53 +0100

    Kings-path hw

---
+++ hw/kings-path/dummy.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MainNamespace
+{
+  class MainClass
+  {
+    static void Main()
+    {
+      Console.WriteLine(-1);
+    }
+  }
+}
---
+++ hw/kings-path/run.sh
@@ -0,0 +1,2 @@
+mcs -out:kings-path/kings-path.exe kings-path/kings-path.cs;
+cat kings-path/input.txt | mono kings-path/kings-path.exe;
