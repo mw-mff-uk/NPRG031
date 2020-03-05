e12b9e14aa2a0981570bfe22c013900a729478d0
Milan Wikarski <milan@wikarski.sk>
2020-03-05 16:41:47 +0100

    Added InputReader and XY classes

--- hw/.templates/program.cs
+++ hw/.templates/program.cs
@@ -2,23 +2,73 @@ using System;
 
 namespace MainNamespace
 {
+  class Char
+  {
+    private int _value;
+    public int Value { get { return this._value; } }
+
+    public bool IsEmpty
+    {
+      get
+      {
+        return (
+          this._value == ' ' ||
+          this._value == '\n' ||
+          this._value == '\t' ||
+          this._value == '\r' ||
+          this._value == '\v'
+        );
+      }
+    }
+
+    public bool IsNum
+    {
+      get
+      {
+        return (this._value >= '0' && this._value <= '9');
+      }
+    }
+
+    public bool IsAlph
+    {
+      get
+      {
+        return (
+          (symbol >= 'a' && symbol <= 'z') ||
+          (symbol >= 'A' && symbol <= 'A')
+        );
+      }
+    }
+
+    public Char(int value)
+    {
+      this._value = value;
+    }
+  }
+
   class InputReader
   {
+    private int[] _buffer;
+
+
     public static int ReadInt()
     {
       int factor = 1;
-      int z = Console.Read();
-      while ((z < '0') || (z > '9'))
-      {
-        if (z == '-')
-        {
+      int s = Console.Read();
 
-          factor = -1;
+      while (this)
+
+        while ((z < '0') || (z > '9'))
+        {
+          if (z == '-')
+          {
+
+            factor = -1;
+            z = Console.Read();
+            break;
+          }
           z = Console.Read();
-          break;
         }
-        z = Console.Read();
-      }
 
       if ((z < '0') || (z > '9'))
         throw new Exception("Invalid character after minus sign");
@@ -31,6 +81,11 @@ namespace MainNamespace
       }
 
       return factor * x;
+    }
+
+    public InputReader()
+    {
+      this._buffer = new int[100];
     }
   }
 
