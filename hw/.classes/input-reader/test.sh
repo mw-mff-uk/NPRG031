c12d006ad15ef7d42bd18987e684543b071d5a95
Milan Wikarski <milan@wikarski.sk>
2020-03-09 14:13:44 +0100

    New file structure

--- hw/.classes/input-reader/test.sh
+++
@@ -1,2 +0,0 @@
-csc classes/input-reader/input-reader.cs -out:classes/input-reader/input-reader.exe
-cat classes/input-reader/test.txt | mono classes/input-reader/input-reader.exe
--- hw/new-hw.sh
+++
@@ -1,7 +0,0 @@
-if [ "$1" = "" ]; then
-  echo "No arguments provided"
-  exit 1
-else
-  mkdir $1
-  cat "./.templates/program.cs" > "$1/$1.cs"
-fi
