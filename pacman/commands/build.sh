d87f6bff25352ab0b27890de72fa774e81e032ec
Milan Wikarski <milan@wikarski.sk>
2020-07-10 10:55:37 +0200

    Movement

--- pacman/commands/build.sh
+++ pacman/commands/build.sh
@@ -4,9 +4,10 @@ dir=$( dirname "$(realpath "$0")" )
 root="$dir/.."
 
 csc \
-  "$root/src/main.cs" \
+  "$root/src/utils/*.cs" \
+  "$root/src/pacman.cs" \
   "$root/src/game.cs" \
-  "$root/src/utils/linked-list.cs" \
+  "$root/src/main.cs" \
   -r:System.Windows.Forms.dll \
   -r:System.Drawing.dll \
   -out:out/pacman.exe
--- pacman/src/game.cs
+++ pacman/src/game.cs
@@ -7,21 +7,49 @@ namespace MainNamespace
 {
   class Game
   {
+    private const int TICKS_PER_SECOND = 20;
+    public const int WIDTH = 400;
+    public const int HEIGHT = 534;
+    public const int CORRIDOR_SIZE = 28;
+    public const int CORRIDOR_GAP = 2;
+    private const int N_STATES = 3;
+    private const int STATE_BEFORE_START = 0;
+    private const int STATE_PLAYING = 1;
+    private const int STATE_GAME_OVER = 2;
+    private int state = Game.STATE_BEFORE_START;
     private Form board;
     private LinkedList<Control> toDispose;
-    private void OnScreenChange()
+    private Pacman pacman;
+    private Timer timer;
+    private TextBox keyboardHandler;
+    private int gapVertical;
+    private int gapHorizontal;
+    private void NextState(object sender = null, EventArgs e = null)
     {
       while (!this.toDispose.Empty)
         this.toDispose.ExtractFirst().Dispose();
+
+      this.state = (this.state + 1) % Game.N_STATES;
+
+      switch (this.state)
+      {
+        case Game.STATE_BEFORE_START: this.WelcomeScreen(); break;
+        case Game.STATE_PLAYING: this.GameScreen(); break;
+        case Game.STATE_GAME_OVER: this.GameOverScreen(); break;
+      }
+
+      this.keyboardHandler.Focus();
     }
-    private void GameScreen(object sender, EventArgs e)
+    private void GameScreen()
     {
-      this.OnScreenChange();
+      this.pacman = new Pacman();
+      this.pacman.Spawn(this.board);
+      this.toDispose.InsertLast(this.pacman);
+
+      timer.Enabled = true;
     }
     private void WelcomeScreen()
     {
-      this.OnScreenChange();
-
       Button btn = new Button();
 
       btn.Text = "New Game";
@@ -31,13 +59,57 @@ namespace MainNamespace
       btn.Width = 100;
       btn.Height = 40;
       btn.Parent = this.board;
-      btn.Click += this.GameScreen;
+      btn.Click += this.NextState;
 
       // Fill toDispose Queue
       this.toDispose.InsertLast(btn);
     }
+    private void GameOverScreen()
+    {
+
+    }
+    private void Tick(object sender, EventArgs e)
+    {
+      if (this.state == Game.STATE_PLAYING)
+      {
+        this.pacman.Move();
+      }
+    }
+    private void OnKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+    {
+      if (this.state == Game.STATE_PLAYING)
+      {
+        switch (e.KeyCode)
+        {
+          case Keys.Right:
+            this.pacman.TurnRight();
+            break;
+
+          case Keys.Left:
+            this.pacman.TurnLeft();
+            break;
+
+          case Keys.Up:
+            this.pacman.TurnUp();
+            break;
+
+          case Keys.Down:
+            this.pacman.TurnDown();
+            break;
+        }
+      }
+    }
     public void Run()
     {
+      this.board.Width = Game.WIDTH + 100;
+      this.board.Height = Game.HEIGHT + 100;
+
+      this.gapHorizontal = (this.board.ClientRectangle.Height - Game.HEIGHT) / 2;
+      this.gapVertical = (this.board.ClientRectangle.Width - Game.WIDTH) / 2;
+
+      Console.WriteLine("H: {0} | {1}", this.board.ClientRectangle.Width, this.gapHorizontal);
+      Console.WriteLine("V: {0} | {1}", this.board.ClientRectangle.Height, this.gapVertical);
+
       this.WelcomeScreen();
       Application.Run(this.board);
     }
@@ -45,6 +117,18 @@ namespace MainNamespace
     {
       this.board = new Form();
       this.toDispose = new LinkedList<Control>();
+
+      this.keyboardHandler = new TextBox();
+      this.keyboardHandler.Width = 0;
+      this.keyboardHandler.Height = 0;
+      this.keyboardHandler.Parent = this.board;
+      this.board.ActiveControl = this.keyboardHandler;
+      this.keyboardHandler.KeyDown += this.OnKeyDown;
+
+      this.timer = new Timer();
+      timer.Interval = 1000 / Game.TICKS_PER_SECOND;
+      timer.Tick += new System.EventHandler(this.Tick);
+      timer.Enabled = false;
     }
   }
 }
