29583118fd6e8a1ddee1419d0e85548908b12f32
Milan Wikarski <milan@wikarski.sk>
2020-07-16 18:57:31 +0200

    .gitignore moved

--- pacman/map-builder/commands/build.sh
+++ pacman/map-builder/commands/build.sh
@@ -6,9 +6,3 @@ root="$dir/.."
 csc \
   "$root/src/*.cs" \
   -out:"$root/map-builder.exe"
-
-rm "$root/pacman.tar.gz"
-tar -zcvf "$root/pacman.tar.gz" "$root"
-
-rm "$root/documentation.pdf"
-mdpdf "$root/readme.md" "$root/documentation.pdf"
