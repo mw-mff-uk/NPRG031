880e56da38cc2b0730f7886746a9aae84c1a0a52
Milan Wikarski <milan@wikarski.sk>
2020-07-17 11:15:14 +0200

    map-builder

--- pacman/map-builder/src/map-builer.cs
+++ pacman/map-builder/src/map-builer.cs
@@ -4,10 +4,44 @@ using System.Drawing;
 
 namespace MapBuilderNamespace
 {
+  class Box
+  {
+    public int Left;
+    public int Top;
+    public int Width;
+    public int Height;
+    public int Right { get => this.Left + this.Width - 1; }
+    public int Bottom { get => this.Top + this.Height - 1; }
+    public Box(int left, int top, int width, int height)
+    {
+      this.Left = left;
+      this.Top = top;
+      this.Width = width;
+      this.Height = height;
+    }
+  }
   class XY
   {
     public int X;
     public int Y;
+    public bool IsInsideBox(Box box)
+    {
+      return (
+        this.X >= box.Left &&
+        this.X <= box.Right &&
+        this.Y >= box.Top &&
+        this.Y <= box.Bottom
+      );
+    }
+    public XY[] GetNeighbors()
+    {
+      return new XY[] {
+        new XY(this.X + 1, this.Y),
+        new XY(this.X - 1, this.Y),
+        new XY(this.X, this.Y + 1),
+        new XY(this.X, this.Y - 1)
+      };
+    }
     public XY(int x, int y)
     {
       this.X = x;
@@ -36,37 +70,93 @@ namespace MapBuilderNamespace
       this.size++;
     }
   }
+  class BinaryMatrix
+  {
+    private bool[,] items;
+    private int width;
+    public int Width { get => this.width; }
+    private int height;
+    public int Height { get => this.height; }
+    public int Size { get => this.width * this.height; }
+    public Bitmap ToBitmap(Color falseColor, Color trueColor)
+    {
+      Bitmap map = new Bitmap(this.width, this.height);
+
+      for (int x = 0; x < this.width; x++)
+        for (int y = 0; y < this.height; y++)
+          map.SetPixel(x, y, this.items[x, y] ? trueColor : falseColor);
+
+      return map;
+    }
+    public bool this[int x, int y]
+    {
+      get => this.items[x, y];
+      set => this.items[x, y] = value;
+    }
+    public bool this[XY point]
+    {
+      get => this.items[point.X, point.Y];
+      set => this.items[point.X, point.Y] = value;
+    }
+    public BinaryMatrix(int width, int height)
+    {
+      this.width = width;
+      this.height = height;
+
+      this.items = new bool[width, height];
+      for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
+          this[x, y] = false;
+    }
+  }
+  class Map
+  {
+    private Bitmap img;
+    private Box box;
+    public Box Box { get => this.box; }
+    private int width;
+    public int Width { get => this.width; }
+    private int height;
+    public int Height { get => this.height; }
+    public BinaryMatrix ScanObstacles(int blueThreshold)
+    {
+      BinaryMatrix obstacles = new BinaryMatrix(this.width, this.height);
+
+      for (int x = 0; x < this.width; x++)
+        for (int y = 0; y < this.height; y++)
+          obstacles[x, y] = this.img.GetPixel(x, y).B >= blueThreshold;
+
+      return obstacles;
+    }
+    public Map(string imgSrc)
+    {
+      this.img = new Bitmap(imgSrc);
+
+      this.width = img.Width;
+      this.height = img.Height;
+
+      this.box = new Box(0, 0, this.width, this.height);
+    }
+  }
   class MainClass
   {
-    const int MIN_BLUE = 200;
-    static void Main()
+    const string FILE_BASE = "/home/wiki/School/NPRG031/pacman/map-builder/";
+    static void PrintObstacleMap(Map map, int blueThreshold)
     {
-      Bitmap img = new Bitmap("/home/wiki/School/NPRG031/pacman/map-builder/background.jpg");
-
-      // Look for obstacles (look for BLUE color)
-      bool[,] obstacles = new bool[img.Width, img.Height];
-      for (int x = 0; x < img.Width; x++)
-        for (int y = 0; y < img.Height; y++)
-          obstacles[x, y] = img.GetPixel(x, y).B >= MIN_BLUE;
-
-      // Create a bitmap of obstacles for illustration
-      Bitmap mask = new Bitmap(img.Width, img.Height);
-      for (int x = 0; x < img.Width; x++)
-        for (int y = 0; y < img.Height; y++)
-          mask.SetPixel(x, y, obstacles[x, y] ? Color.Red : Color.Black);
-      mask.Save("/home/wiki/School/NPRG031/pacman/map-builder/background-mask.jpg");
-
-      // Scan for clusters
-      bool[,] discovered = new bool[img.Width, img.Height];
-      for (int x = 0; x < img.Width; x++)
-        for (int y = 0; y < img.Height; y++)
-          discovered[x, y] = false;
-
+      map
+        .ScanObstacles(blueThreshold)
+        .ToBitmap(Color.Black, Color.Red)
+        .Save(FILE_BASE + "background-mask.jpg");
+    }
+    static void PrintSegmentMap(Map map, int blueThreshold)
+    {
+      BinaryMatrix discovered = new BinaryMatrix(map.Width, map.Height);
+      BinaryMatrix obstacles = map.ScanObstacles(blueThreshold);
       List<Cluster> clusters = new List<Cluster>();
 
-      for (int x = 0; x < img.Width; x++)
+      for (int x = 0; x < map.Width; x++)
       {
-        for (int y = 0; y < img.Height; y++)
+        for (int y = 0; y < map.Height; y++)
         {
           if (!discovered[x, y] && obstacles[x, y])
           {
@@ -81,33 +171,19 @@ namespace MapBuilderNamespace
               XY point = queue.Dequeue();
               cluster.AddPoint(point);
 
-              XY[] points = new XY[] {
-                new XY(point.X + 1, point.Y),
-                new XY(point.X - 1, point.Y),
-                new XY(point.X, point.Y + 1),
-                new XY(point.X, point.Y - 1)
-              };
-
-              foreach (XY neighbor in points)
+              foreach (XY neighbor in point.GetNeighbors())
               {
                 if (
-                  neighbor.X >= 0 &&
-                  neighbor.X < img.Width &&
-                  neighbor.Y >= 0 &&
-                  neighbor.Y < img.Height &&
-                  !discovered[neighbor.X, neighbor.Y] &&
-                  obstacles[neighbor.X, neighbor.Y]
+                  neighbor.IsInsideBox(map.Box) &&
+                  !discovered[neighbor] &&
+                  obstacles[neighbor]
                 )
                 {
-                  discovered[neighbor.X, neighbor.Y] = true;
+                  discovered[neighbor] = true;
                   queue.Enqueue(neighbor);
                 }
               }
-
-              // Console.WriteLine(queue.Count);
             }
-
-            // Console.WriteLine(cluster.Size);
 
             clusters.Add(cluster);
           }
@@ -116,10 +192,10 @@ namespace MapBuilderNamespace
 
       Console.WriteLine("Discovered {0} clusters", clusters.Count);
 
-      Bitmap clustersMap = new Bitmap(img.Width, img.Height);
+      Bitmap clustersMap = new Bitmap(map.Width, map.Height);
 
-      for (int x = 0; x < img.Width; x++)
-        for (int y = 0; y < img.Height; y++)
+      for (int x = 0; x < map.Width; x++)
+        for (int y = 0; y < map.Height; y++)
           clustersMap.SetPixel(x, y, Color.Black);
 
       foreach (Cluster cluster in clusters)
@@ -127,7 +203,46 @@ namespace MapBuilderNamespace
           for (int y = cluster.Top; y <= cluster.Bottom; y++)
             clustersMap.SetPixel(x, y, Color.Red);
 
-      clustersMap.Save("/home/wiki/School/NPRG031/pacman/map-builder/background-segments.jpg");
+      clustersMap.Save(FILE_BASE + "background-segments.jpg");
+    }
+    static void PrintCorridorMap(Map map, XY initial, int blueThreshold)
+    {
+      BinaryMatrix discovered = new BinaryMatrix(map.Width, map.Height);
+      BinaryMatrix corridors = new BinaryMatrix(map.Width, map.Height);
+      BinaryMatrix obstacles = map.ScanObstacles(blueThreshold);
+      Queue<XY> queue = new Queue<XY>();
+
+      queue.Enqueue(initial);
+      discovered[initial.X, initial.Y] = true;
+
+      while (queue.Count > 0)
+      {
+        XY point = queue.Dequeue();
+        corridors[point] = true;
+
+        foreach (XY neighbor in point.GetNeighbors())
+        {
+          if (
+            neighbor.IsInsideBox(map.Box) &&
+            !discovered[neighbor] &&
+            !obstacles[neighbor]
+          )
+          {
+            discovered[neighbor] = true;
+            queue.Enqueue(neighbor);
+          }
+        }
+      }
+
+      corridors.ToBitmap(Color.Black, Color.Green).Save(FILE_BASE + "background-corridors.jpg");
+    }
+    static void Main()
+    {
+      Map map = new Map(FILE_BASE + "background.jpg");
+
+      PrintObstacleMap(map, 180);
+      PrintSegmentMap(map, 180);
+      PrintCorridorMap(map, new XY(120, 789), 80);
     }
   }
 }
